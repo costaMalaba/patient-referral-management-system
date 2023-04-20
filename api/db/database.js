19801ff35be4684875034f7dd9ad177895d89c61
cymstar <costantineyohana1999@gmail.com>
2023-04-20 15:23:53 +0300

    Done some changes

--- api/db/database.js
+++ api/db/database.js
@@ -1,10 +1,13 @@
-require('dotenv').config();
-const mysql = require('mysql');
+import mysql from "mysql";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const con = mysql.createConnection({
     host: process.env.HOST,
-    user: process.env.USER,
+    user: process.env.DB_USER,
     password: process.env.PASSWORD,
+    port: process.env.DB_PORT,
     database: process.env.DATABASE
 });
 
@@ -17,4 +20,4 @@ con.connect(error => {
     }
 });
 
-module.exports = con;
+export default con;
--- api/index.js
+++ api/index.js
@@ -1,24 +1,32 @@
-const express =  require('express');
-const { con } = require('./db/database.js');
-require('dotenv').config();
+import express from "express";
+import con from "./db/database.js"
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-PORT = process.env.PORT || 5000
 
-app.get("/", (req, res) => {    
-    const q = 'SELECT * FROM patient';
-    con.query(q, (err, results) => {
-        if(error)
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000
+
+app.get("/", (req, res) => {
+    res.json("Hello PRMS");
+})
+
+app.get("/patient", (req, res) => {    
+    const q = "SELECT * FROM patient";
+    con.query(q, (err, data) => {
+        if(err)
         {
             return res.json(err)
         }
         else
         {
-            return res.json(results);
+            return res.json(data);
         }
     });
 })
 
 app.listen(PORT, (
-    console.log(`App listening on port ${process.env.PORT}`)
+    console.log(`App listening on port ${PORT}`)
 ));
